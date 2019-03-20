4f9aafa070270cceebc0a56e38391c4801ba3296
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-03-20 17:21:33 +0300

    Update 4quest.rb

--- lesson2/4quest.rb
+++ lesson2/4quest.rb
@@ -1,4 +1,4 @@
-vowels = ["a", "e", "i", "o", "u"] #указываем гласные буквы
+vowels = %w(a e i o u) #указываем гласные буквы
 vowels_hash={} #создаем хеш из гласных 
 #присваиваем каждо букве алфавита индекс начиная с 1
 #выводиться будет буква=>индекс, так как используется хеш (ключ=>значение)
@@ -6,5 +6,4 @@ vowels_hash={} #создаем хеш из гласных
 ("a".."z").each.with_index(1)  do |letter, index|
   vowels_hash[letter] = index if vowels.include?(letter)
 end 
-
 puts vowels_hash
