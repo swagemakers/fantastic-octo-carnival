0576af8b7a1db18c4348a7880e04fbf88ac9e841
Kill la kill <TankistB@Jenneken.local>
2019-03-20 15:22:16 +0300

    yeeyee

---
+++ lesson2/6quest.rb
@@ -0,0 +1,29 @@
+cart = {}
+total = 0
+
+loop do 
+  puts "Введите название товара или 'стоп', чтобы закончить ввод: "
+  title = gets.chomp 
+
+  break if title == 'стоп'
+
+  puts "Введите цену указанного товара: "
+  price = gets.to_f
+
+  puts "Ввелите количество указанного товара: "
+  quantity = gets.to_f
+
+  cart[title] = {price: price, quantity: quantity}
+end
+
+puts cart
+
+cart.each do |key, value|
+  sum = value[:price] * value[:quantity]
+  puts value
+  puts "#{key}: #{value [:ptice]} * #{value [:quantity]} = #{sum}"
+
+  total += sum
+end
+
+  puts "Итого: #{total}"
