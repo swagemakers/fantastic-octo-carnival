a338196341f1da6b7e490fe71eac39e74e66c4f2
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-03-20 17:05:54 +0300

    Update 5quest.rb

--- lesson2/5quest.rb
+++ lesson2/5quest.rb
@@ -9,12 +9,13 @@ month = gets.to_i
 puts"Введите год: "
 year = gets.to_i 
 
-days [1] = 29 if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+leap_year = (year % 4 == 0 && year % 100 != 0) || year % 400 == 0
+days [1] = 29 if leap_year
 
 sum = 0
 for i in 0 .. month-2 
   sum += days[i]
- end 
+end 
 
 sum += date
 
