18e2fb3fc92364d4d2ab82419cccc2feac5b9043
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-03-20 21:10:46 +0300

    Update 1quest.rb

--- lesson2/1quest.rb
+++ lesson2/1quest.rb
@@ -1,15 +1,15 @@
 days_in_month = { 
-    january: 31,
-    february: 28,
-    march: 31,
-    april: 30,
-    may: 31,
-    june: 30,
-    july: 31,
-    august: 31, 
-    september: 30,
-    october: 31, 
-    november: 30,
-    december: 31
-    }
-days_in_month.each {|month, days| puts month if days == 30}
+  january: 31,
+  february: 28,
+  march: 31,
+  april: 30,
+  may: 31,
+  june: 30,
+  july: 31,
+  august: 31, 
+  september: 30,
+  october: 31, 
+  november: 30,
+  december: 31
+}
+days_in_month.each { |month, days| puts month if days == 30 }
