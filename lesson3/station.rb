316868046657a82f7b71e393608195d1335dc0a2
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-03-26 20:40:00 +0300

    Update station.rb

--- lesson3/station.rb
+++ lesson3/station.rb
@@ -3,17 +3,18 @@ class Station
 
   def initialize (station_name)
     @name = name #when creating a new object needs a station name as a parameter
+    @trains = []
   end
 
   def add_train(train)
-    @train << train #adds a train to the station
+    @trains << train #adds a train to the station
   end
 
   def remove_train(train)
-    @train.remove(train) #removes trains from the station
+    @trains.remove(train) #removes trains from the station
   end
 
-  def train_types(type)
-    @train.select { |train| train.type == type } #identifies trains type
+  def trains_types(type)
+    @trains.select { |train| train.type == type } #identifies trains type
   end
 end
