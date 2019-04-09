24c18d8c6a9c26d59628c43e089a30758b35cfd7
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-04-09 22:16:37 +0300

    Update cargo_train.rb

--- lesson4/cargo_train.rb
+++ lesson4/cargo_train.rb
@@ -2,12 +2,15 @@ class CargoTrain < Train #cargo is a subclass of train
 
   attr_reader :number, :type #the number and type of the train are only available for reading
 
-  def initialize (number)
-    super #recieves all the train methods as a subclass
-    @type = "Cargo" #defines the type a cargo
+  def initialize(number)
+    super(number, :cargo) #recieves all the train methods as a subclass
   end
 
-  def attachCarriage (carriage)
+  def attachable_carriage?(carriage)
     carriage.is_a?(CargoCarriage) #adds a carriage only if it's a cargo carriage
+  end
+
+  def to_s
+    super
   end
 end
