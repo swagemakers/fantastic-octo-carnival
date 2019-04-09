c667f46eb2c255ecb476a81e2b8563f5c2d9c16c
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-04-09 15:23:02 +0300

    Update main.rb

--- lesson4/main.rb
+++ lesson4/main.rb
@@ -1,11 +1,11 @@
-require_relative 'stationClass.rb'
-require_relative 'routeClass.rb'
-require_relative 'trainClass.rb'
-require_relative 'PassengerTrain.rb'
-require_relative 'CargoTrain.rb'
-require_relative 'Carriage.rb'
-require_relative 'PassengerCarriage.rb'
-require_relative 'CargoCarriage.rb'
+require_relative 'station.rb'
+require_relative 'route.rb'
+require_relative 'train.rb'
+require_relative 'passenger_train.rb'
+require_relative 'cargo_train.rb'
+require_relative 'carriage.rb'
+require_relative 'passenger_carriage.rb'
+require_relative 'cargo_carriage.rb'
 
 class Main
   def menu
