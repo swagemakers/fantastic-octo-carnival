9e7db25309e140db350e3cd4172982f67061a3ea
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-04-10 20:23:14 +0300

    Update carriage.rb

--- lesson4/carriage.rb
+++ lesson4/carriage.rb
@@ -2,19 +2,4 @@ class Carriage
 
   attr_reader :train
 
-  def initialize
-    @attached = false
-  end
-
-  def attached?
-    @attached = false
-  end
-
-  def attached!
-    @attached = true
-  end
-
-  def unattached
-    @attached = false
-  end
 end
