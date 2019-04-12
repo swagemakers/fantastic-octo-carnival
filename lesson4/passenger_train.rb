e6c5f5fe3e124138efbd89e466b0a3f184b4db81
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-04-12 15:41:40 +0300

    Update passenger_train.rb

--- lesson4/passenger_train.rb
+++ lesson4/passenger_train.rb
@@ -3,11 +3,11 @@ class PassengerTrain < Train #passanger is a train subclass
   attr_reader :number, :type #the number and type of the train are only available for reading
 
   def initialize(number)
-    super (number, :passenger) #recieves all the train methods as its subclass
-    @type = "Passanger" #defines the type as passanger
+    super(number, :passenger) #recieves all the train methods as its subclass
+    @type = "Passenger" #defines the type as passanger
   end
 
-  def attachable_carriage? (carriage)
-    carriage.is_a? (PassengerCarriage) #attaches a carriage only if it's a passanger one
+  def attachable_carriage?(carriage)
+    carriage.is_a?(PassengerCarriage) #attaches a carriage only if it's a passanger one
   end
 end
