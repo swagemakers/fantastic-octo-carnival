4442132c00fbbc1fbc3cc2259fa3ad395894ed63
swagemakers <48688649+swagemakers@users.noreply.github.com>
2019-04-23 21:13:08 +0300

    Update trains.rb

--- lesson5/trains.rb
+++ lesson5/trains.rb
@@ -1,10 +1,10 @@
 class Train
   attr_reader :speed, :type, :carriages, :number
 
-  @@all = {:number => :train}
+  @@all = {}
 
   def self.find(number)
-    return unless @@all.include?(train)
+    @@all[number]
   end
 
   def initialize(number, type)
@@ -12,7 +12,7 @@ class Train
     @carriages = []
     @speed = 0
     @type = type
-    @@all << self
+    @@all[number] = self
   end
 
   def stop
