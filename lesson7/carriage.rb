57a62cf8f03e84f53eb60a39a9c7623c132df54c
Kill la kill <swagemakers.mp@gmail.com>
2019-05-14 21:05:10 +0300

    changed raise position

--- lesson7/carriage.rb
+++ lesson7/carriage.rb
@@ -8,8 +8,8 @@ class Carriage
   end
 
   def occupy_space(value)
-    @occupied_space =+ value
     raise NOT_ENOUGH_SPACE if value > @available_space
+    @occupied_space =+ value
   end
 
   def available_space
